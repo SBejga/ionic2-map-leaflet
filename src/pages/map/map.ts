a02b21dceb83bbd4cb261951f3c3bddbf9e6d24d
Sebastian Bejga <sebastian@bejga.de>
2017-06-23 21:56:03 +0200

    show geolocation by marker

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 import L from "leaflet";
 
-/*
-  Generated class for the Map page.
+//Coordinates, PositionError
+import { Geolocation, Geoposition, GeolocationOptions } from '@ionic-native/geolocation';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+const GEOLOCATION_OPTIONS: GeolocationOptions = {
+   maximumAge: 3000, timeout: 5000, enableHighAccuracy: true
+};
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
@@ -16,8 +17,18 @@ import L from "leaflet";
 export class MapPage {
   map: L.Map;
   center: L.PointTuple;
+  following: Boolean;
+  positionMarker: L.Marker;
+  positionAccuracyCircle: L.Circle;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private geolocationSubscription;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private geolocation: Geolocation,
+    public toastCtrl: ToastController
+  ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
@@ -28,6 +39,14 @@ export class MapPage {
     
     //setup leaflet map
     this.initMap();
+
+    //Show Toast how to enable geolocation
+    let toast = this.toastCtrl.create({
+      message: 'To locate your position, press icon button in top right corner ',
+      position: 'middle',
+      showCloseButton: true
+    });
+    toast.present();
   }
 
   initMap() {
@@ -39,6 +58,46 @@ export class MapPage {
     //Add OSM Layer
     L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
       .addTo(this.map);
+  }
+
+  toggleFollow() {
+    this.following = !this.following;
+
+    if (this.following) {
+      this.startFollow();
+    } else {
+      this.stopFollow();
+    }
+  }
+
+  startFollow() {
+    this.geolocationSubscription = this.geolocation.watchPosition(GEOLOCATION_OPTIONS)
+      //.filter((p) => p.coords !== undefined) //Filter Out Errors
+      .subscribe(position => {
+        this.updateGeoposition(position);
+      });
+  }
+
+  stopFollow() {
+    this.geolocationSubscription.unsubscribe();
+  }
+
+  updateGeoposition(position: Geoposition) {
+    console.log(position.coords.longitude + ' ' + position.coords.latitude);
+    
+    //create Point
+    let latlng = {lat: position.coords.latitude, lng: position.coords.longitude, date: new Date()};
+
+    if (this.positionMarker) {
+      this.positionMarker.setLatLng(latlng);
+      this.positionAccuracyCircle.setLatLng(latlng).setRadius(position.coords.accuracy);
+    } else {
+      this.positionMarker = L.marker(latlng).addTo(this.map);
+      this.positionAccuracyCircle = L.circle(latlng, {radius: position.coords.accuracy}).addTo(this.map);
+    }
+
+    //Set Center
+    this.map.setView(latlng, 13);
   }
 
 }
